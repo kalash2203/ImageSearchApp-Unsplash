c0068a69e596d42c8adf5c0a3b4c781c17a73c90
Tanmay <tanmaysingh0311@gmail.com>
2022-02-01 18:37:14 +0530

    Initial commit

---
+++ app/src/main/java/com/kalashsonker/android/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.kalashsonker.android
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_main) as NavHostFragment
+        navController = navHostFragment.findNavController()
+
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+//
+//              bottomNavigationView.setupWithNavController(navController)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+}
---
+++ app/src/main/java/com/kalashsonker/android/api/UnsplashResponse.kt
@@ -0,0 +1,7 @@
+package com.kalashsonker.android.api
+
+import com.kalashsonker.android.data.UnsplashPhoto
+
+data class UnsplashResponse(
+    val results: List<UnsplashPhoto>
+)
